cc9d2e503463a505da0a31a093ef70f4acbe760c
masolangeg <marina.solange.g@gmail.com>
2024-03-20 20:05:54 -0300

    change ui botom img

--- RO-PA-SC-LI-SP/src/App.jsx
+++ RO-PA-SC-LI-SP/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
-
+import lizard from "./assets/lizard.png";
+import rock from "./assets/rock.png";
+import scissor from "./assets/scissor.png";
+import spock from "./assets/spock.png";
+import paper from "./assets/paper.png";
 // const options = [
 //   { id: 0, name: "Rock", emoji: "🪨", beats: [2, 3] },
 //   { id: 1, name: "Paper", emoji: "📄", beats: [0, 4] },
@@ -29,21 +33,41 @@ function App() {
   const [language, setLanguage] = useState(true);
 
   const options = [
-    { id: 0, name: language ? "Piedra" : "Rock", emoji: "🪨", beats: [2, 3] },
-    { id: 1, name: language ? "Papel" : "Paper", emoji: "📄", beats: [0, 4] },
+    {
+      id: 0,
+      name: language ? "Piedra" : "Rock",
+      // emoji: "🪨",
+      image: rock,
+      beats: [2, 3],
+    },
+    {
+      id: 1,
+      name: language ? "Papel" : "Paper",
+      // emoji: "📄",
+      image: paper,
+      beats: [0, 4],
+    },
     {
       id: 2,
       name: language ? "Tijera" : "Scissor",
-      emoji: "✂️",
+      // emoji: "✂️",
+      image: scissor,
       beats: [1, 3],
     },
     {
       id: 3,
       name: language ? "Lagarto" : "Lizard",
-      emoji: "🐊",
+      // emoji: "🐊",
+      image: lizard,
       beats: [1, 4],
     },
-    { id: 4, name: language ? "Spock" : "Spock", emoji: "🖖🏻", beats: [2, 0] },
+    {
+      id: 4,
+      name: language ? "Spock" : "Spock",
+      // emoji: "🖖🏻",
+      image: spock,
+      beats: [2, 0],
+    },
   ];
 
   const results = JSON.parse(localStorage.getItem("gameResults"));
@@ -74,8 +98,8 @@ function App() {
     if (userChoice !== null) {
       setUserMessage(
         language
-          ? `Tu has elegido ${options[userChoice]?.emoji} - ${options[userChoice]?.name}`
-          : `You choice ${options[userChoice]?.emoji} - ${options[userChoice]?.name}`
+          ? `Tu has elegido ${options[userChoice]?.image}  ${options[userChoice]?.name}`
+          : `You choice ${options[userChoice]?.image}  ${options[userChoice]?.name}`
       );
     }
   }, [userChoice]);
@@ -84,8 +108,8 @@ function App() {
     if (computerChoice !== null) {
       setComputerMessage(
         language
-          ? `Sheldon ha elegido ${options[computerChoice]?.emoji} - ${options[computerChoice]?.name}`
-          : `Sheldon choice ${options[computerChoice]?.emoji} - ${options[computerChoice]?.name}`
+          ? `Sheldon ha elegido ${options[computerChoice]?.image} - ${options[computerChoice]?.name}`
+          : `Sheldon choice ${options[computerChoice]?.image} - ${options[computerChoice]?.name}`
       );
     }
   }, [computerChoice]);
@@ -136,17 +160,17 @@ function App() {
   const getClass = (id) => {
     switch (id) {
       case 0:
-        return "px-8 py-8 m-4 text-xl font-bold text-white bg-[#BF1F5A] hover:bg-[#8D0C3C] rounded-full";
+        return "px-10 py-10 m-4 bg-[#BF1F5A] hover:bg-[#8D0C3C] rounded-full ";
       case 1:
-        return "px-8 py-8 m-4 text-xl font-bold text-white bg-[#A61B1B] hover:bg-[#811515] rounded-full";
+        return "px-10 py-10 m-4 bg-[#A61B1B] hover:bg-[#811515] rounded-full ";
       case 2:
-        return "px-8 py-8 m-4 text-xl font-bold text-white bg-[#038C4C] hover:bg-[#01562E] rounded-full";
+        return "px-10 py-10 m-4 bg-[#038C4C] hover:bg-[#01562E] rounded-full ";
       case 3:
-        return "px-8 py-8 m-4 text-xl font-bold text-white bg-[#103C59] hover:bg-[#08263A] rounded-full";
+        return "px-10 py-10 m-4 bg-[#103C59] hover:bg-[#08263A] rounded-full ";
       case 4:
-        return "px-8 py-8 m-4 text-xl font-bold text-white bg-[#6966A2] hover:bg[#4E4C77] rounded-full";
+        return "px-10 py-10 m-4 bg-[#6966A2] hover:bg-[#4E4C77] rounded-full ";
       default:
-        "px-8 py-8 m-4 text-xl font-bold text-white bg-[#F28627] hover:bg[#CA6F1F] rounded-full";
+        "px-10 py-10 m-4 bg-[#F28627] hover:bg[#CA6F1F] rounded-full";
     }
   };
 
@@ -187,15 +211,6 @@ function App() {
                   {options[computerChoice]?.name} ❎
                 </p>
               )}
-
-              <div>
-                <p>
-                  {" "}
-                  {language
-                    ? `Resultados de la partida: Jugador: ${results.player} Sheldon: ${results.sheldon} Empates: ${results.ties}`
-                    : `Game results: Player: ${results.player} Sheldon: ${results.sheldon} Ties: ${results.ties}`}{" "}
-                </p>
-              </div>
             </div>
           )}
 
@@ -208,22 +223,37 @@ function App() {
               onClick={() => handlePlay(option.id)}
               title={option.name}
             >
-              {option.emoji}
+              <img
+                src={option.image}
+                width={40}
+                style={{ objectFit: "cover" }}
+              />
             </button>
           ))}
-
         </div>
-       { result !== null &&  <button
-            className="bg-pink-500 hover:bg-pink-700 text-black font-semibold py-2 px-4 mt-4 border-b-4 border-pink-700"
+        <div>
+          {result !== null && (
+            <p
+            className="bg-[#F28627]  text-white font-semibold py-2 px-4 rounded-full "
+            >
+              {" "}
+              {language
+                ? `Resultados de la partida: Jugador: ${results.player} Sheldon: ${results.sheldon} Empates: ${results.ties}`
+                : `Game results: Player: ${results.player} Sheldon: ${results.sheldon} Ties: ${results.ties}`}{" "}
+            </p>
+          )}
+        </div>
+        {result !== null && (
+          <button
+            className="bg-[#A61B1B] hover:bg-[#811515] text-white font-semibold py-2 px-4 mt-4 border-b-4 border-[#811515] animate-pulse rounded-full "
             onClick={reset}
           >
             {language ? "Jugar de nuevo" : "Play again"}
-          </button>}
+          </button>
+        )}
       </div>
     </div>
   );
 }
 
 export default App;
-
-// https://youtu.be/6YtPXjY30Qc?si=TWW0CEqCl9EZR2XW
